b7f76ad614c0e1a27e36d017e70e7e9731ba89f5
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-09-05 23:25:39 +0300

    added view and template all connection on dev

--- intranet/intranet/connection_on_device/urls.py
+++ intranet/intranet/connection_on_device/urls.py
@@ -5,5 +5,6 @@ from connection_on_device import views
 urlpatterns = [
 	path('', views.index, name='connections'),
 	path('<int:id_dev>/path', views.path_to, name='path_to_dev'),
+	path('<int:id_dev>/all_connection', views.all_connection, name='all_connection'),
 	path('<int:id_dev>/', views.on_device, name='connections_on_dev'),
 ]
--- intranet/intranet/connection_on_device/views.py
+++ intranet/intranet/connection_on_device/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
+import re
+
 from connection_on_device.models import ConnectionOnDevice
 from models_device.models import Device
 
@@ -81,4 +83,55 @@ def path_to(request, id_dev):
         if if_loop > 15:
             return HttpResponse('path not found')
 
+def serch_connect(connect_on_port):
+    ip_pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    _search = re.search(ip_pattern, connect_on_port)
+    if _search is not None:
+        result = Device.objects.get(ip=_search.group())
+    else:
+        result = False
+    return result
 
+
+
+def all_connection_port(dev, connections_dev=[]):
+    all_connection = ConnectionOnDevice.objects.filter(id_dev=dev)
+    connections = []
+    list_connection_lower_dev = []
+
+    for connection in all_connection:
+        _search = serch_connect(connection.connected)
+        if _search:
+            list_connection_lower_dev.append(_search)
+        vlan = f'/{connection.vlan}vlan'
+
+        if connection.connected == 'UP':
+            connection.connected = f'UP to {dev.up_connect_port} port on {dev.model.type} {dev.up_connect_ip}'
+ 
+        if connection.vlan != "Null":
+            connection.vlan = vlan
+        else:
+            connection.vlan = ""
+        connections.append(connection)
+    connections_dev.append((dev, COLORS[dev.model.type], sorted(connections, key=lambda x: x.port)))
+
+    for _dev in list_connection_lower_dev:
+        all_connection_port(_dev)
+
+    return connections_dev
+
+
+
+def all_connection(request, id_dev):
+
+    try:
+        dev = Device.objects.get(pk=id_dev)
+    except Device.DoesNotExist:
+        raise Http404('Device not found!')
+
+    connections = all_connection_port(dev)   
+    
+    
+    return render(request, "all_connection_from.html", {
+        "connections":connections, 
+        })
