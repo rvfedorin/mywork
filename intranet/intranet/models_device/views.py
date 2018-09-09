47a75aefb68306cf5e78aa2b0fc759290087583b
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-09-09 17:14:27 +0300

    paginator is placed in a separate func

--- intranet/intranet/models_device/views.py
+++ intranet/intranet/models_device/views.py
@@ -7,22 +7,28 @@ from cities.models import Cities, REGIONS
 # Create your views here.
 
 
+def my_paginator(request, obj_list, num_list=5):
+    try:
+        page_num = request.GET['page']
+    except KeyError:
+        page_num = 1
+
+    paginator = Paginator(obj_list, num_list)
+
+    try:
+        all_dev = paginator.page(page_num)
+    except InvalidPage:
+        all_dev = paginator.page(1)
+
+    return all_dev
+
+
 def index(request):
     regions = []
     for _reg in REGIONS:
         regions.append(_reg[1])
 
-    try:
-    	page_num = request.GET['page']
-    except KeyError:
-    	page_num = 1
-
-    paginator = Paginator(Device.objects.all().order_by('city'), 5)
-
-    try:
-    	all_dev = paginator.page(page_num)
-    except InvalidPage:
-    	all_dev = paginator.page(1)
+    all_dev = my_paginator(request, Device.objects.all().order_by('city'))
 
     return render(request, "dev_list.html", {
         "devices":all_dev, 
@@ -42,23 +48,14 @@ def region(request, region):
     for _dev in reg:
         all_dev += list(Device.objects.filter(city=_dev))
 
-    try:
-    	page_num = request.GET['page']
-    except KeyError:
-    	page_num = 1
-
-    paginator = Paginator(all_dev, 5)
-
-    try:
-    	all_dev = paginator.page(page_num)
-    except InvalidPage:
-    	all_dev = paginator.page(1)
+    all_dev_page = my_paginator(request, all_dev)
 
     return render(request, "dev_list.html", {
-        "devices":all_dev, 
+        "devices":all_dev_page, 
         "cities":reg,
         "region":region,
         })
+
 
 def city(request, region, city):
 
@@ -69,19 +66,10 @@ def city(request, region, city):
 
     all_dev = Device.objects.filter(city=reg)
 
-    try:
-    	page_num = request.GET['page']
-    except KeyError:
-    	page_num = 1
-
-    paginator = Paginator(all_dev, 5)
-    try:
-    	all_dev = paginator.page(page_num)
-    except InvalidPage:
-    	all_dev = paginator.page(1)
+    all_dev_page = my_paginator(request, all_dev)
 
     return render(request, "dev_list.html", {
-        "devices":all_dev, 
+        "devices":all_dev_page, 
         "region":region,
         "city":city,
         })
